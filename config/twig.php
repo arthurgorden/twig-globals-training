82898139818fbd1cc71bb26f0d9e2b495d85e30b
agorden <arthur.gorden@live.fr>
2022-04-07 16:18:05 +0200

    first commit

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale author à l'initialisation de l'objet Twig
+
+$mailContact = 'admin@theawesomewebsite.awesomeness';
+
+$twig->addGlobal('contactEmail', $mailContact);
